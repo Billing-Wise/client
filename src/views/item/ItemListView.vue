36118fc7808e678aecbe465f473ad572323aa70a
김한주 <rlagkswn37@naver.com>
2024-07-18 18:55:14 +0900

    [K5P-51] [feat] 아이콘 버튼, 검색 컴포넌트

--- src/views/item/ItemListView.vue
+++ src/views/item/ItemListView.vue
@@ -1,18 +1,50 @@
 <template>
-  <div>
-    <h1>상품 목록</h1>
+  <div class="root-container">
+    <div class="top-btn-box">
+      <ThemeIconBtnVue title="상품 등록" icon="bi bi-plus-square"/>
+      <SearchInputVue title="상품명 검색" :modelValue="itemName"/>
+    </div>
+    <div class="table-box">
+      <div>300</div>
+      <div>12341345</div>
+    </div>
   </div>
 </template>
 
 <script>
+import ThemeIconBtnVue from '@/components/common/btn/ThemeIconBtn.vue';
+import SearchInputVue from '@/components/common/input/SearchInput.vue';
+
 export default {
-  name : 'ItemListView'
+  name : 'ItemListView',
+  components: {
+    ThemeIconBtnVue,
+    SearchInputVue
+  },
+  data() {
+    return {
+      itemName: ""
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
-  div{
+  .root-container {
+    @include flex-box(column, space-between, 20px);
+    background: $back-color;
     width: 100%;
     height: 100%;
+    padding: 30px 40px
+  }
+  .top-btn-box {
+    @include flex-box(row, space-between, 20px);
+    width: 100%;
+  }
+  .table-box {
+    @include flex-box(column, space-between, 20px);
+    background: white;
+    width: 100%;
+    flex-grow: 1;
   }
 </style>
