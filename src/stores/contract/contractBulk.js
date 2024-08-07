7061ca542bc38d5eadb2eb7d8bd8fea06b29ba99
김한주 <rlagkswn37@naver.com>
2024-08-07 09:33:57 +0900

    [K5P-80] [fix] 엑셀 업로드 사진 주소 변경

--- src/stores/contract/contractBulk.js
+++ src/stores/contract/contractBulk.js
@@ -3,7 +3,7 @@ import { defineStore } from 'pinia'
 export const useContractBulkStore = defineStore('contractBulk', {
   state() {
     return {
-      exampleImg: 'example_contract.png',
+      exampleImg: 'https://t5-bucket.s3.ap-northeast-2.amazonaws.com/excel/%EA%B3%84%EC%95%BD_%EC%97%91%EC%85%80_%EC%98%88%EC%8B%9C.png',
       download: 'https://t5-bucket.s3.ap-northeast-2.amazonaws.com/excel/%EA%B3%84%EC%95%BD%EB%93%B1%EB%A1%9D.xlsx',
       fileName: '계약등록.xlsx',
       file: null,
